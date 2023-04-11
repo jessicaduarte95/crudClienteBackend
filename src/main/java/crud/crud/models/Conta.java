b4bf0499ce4a336b43f3251c14effd476a82355e
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-04-11 15:39:02 -0300

    Ajuste

--- src/main/java/crud/crud/models/Conta.java
+++ src/main/java/crud/crud/models/Conta.java
@@ -16,7 +16,7 @@ public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idConta;
-    private Long numConta;
+    private String numConta;
 
     @ManyToOne
     @JoinColumn(name = "idCliente", nullable = false)
